5f3df0d1fab7e729ca6db23e81c746757b3845f5
Vadim <snooperv@gmail.com>
2022-06-29 21:29:12 +0500

    Первое занятие

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+/* Нащ\ш собственный тип данных, который нужен для работы функции format() */
+interface formatUser {
+  firstName: string;
+  secondname: string;
+  patronymic?: string;
+}
 
-// export default App;
+function App() {
+  /* Функция (просто выглядит немного по-другому) */
+  const format = (user: formatUser) => {
+    let name = user.firstName + " " + user.secondname;
+    if (user.patronymic) {
+      name += " " + user.patronymic;
+    }
+    return name;
+  };
+
+  /* Переменная user - словарь, который имеет три поля (ключа) */
+  const user = {
+    firstName: "Ivan",
+    patronymic: "Ivanovich",
+    secondname: "Ivanov",
+  };
+
+  const user2 = {
+    firstName: "Petr",
+    secondname: "Petrov",
+  };
+
+  /* Константа (неизменяемая переменная) */
+  const elem = <h1>Привет, {format(user)}!</h1>;
+
+  return elem; /* Отображаем элемент elem */
+}
+
+export default App;
