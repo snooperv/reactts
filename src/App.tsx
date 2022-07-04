82fe11eda3af8dd127daaab2c0384f54fcd0cca3
BragaMaria <brag4.m@yandex.com>
2022-07-04 10:30:17 +0500

    Добавили функцию: Имя разработчика

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-interface nameName {
-  userName: string;
-  secondName: string;
-}
 
 function App() {
-  const formatName = (user: nameName) => {
-    return user.userName + " " + user.secondName;
-  };
-  const name = {
-    userName: "Maria",
-    secondName: "Braga",
-  };
-  return <h1>Имя разработчика: {formatName(name)}</h1>;
+  return (
+    <div>
+      <button>Нажми меня</button>
+      <br />
+      счетчик
+    </div>
+  );
 }
 
 export default App;
