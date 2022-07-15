1eee556901ccfa8e51a04dba85fd40e7e4b2b860
BragaMaria <brag4.m@yandex.com>
2022-07-15 11:07:03 +0500

    Создали переменные

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
 import React, { useState } from "react";
 
+interface IData {
+  name?: string;
+  surname?: string;
+  password?: string;
+  email?: string;
+  city?: string;
+}
+
 function App() {
- 
-  return (
+  const [perconalData, setPerconalData] = useState<IData>();
+  const mainForm:JSX.Element= (
     <div>
       <h3>Вы не авторизованны, пройдите небольшую регистрацию</h3>
       <form>
         <label>
-          Имя <input type="text" />
+          Имя{" "}
+          <input
+            type="text"
+            required
+            onChange={(event) => setPerconalData({ name: event.target.value })}
+          />
         </label>{" "}
         <br />
         <label>
-          Фамилия <input type="text" />
+          Фамилия{" "}
+          <input
+            type="text"
+            required
+            onChange={(event) =>
+              setPerconalData({ surname: event.target.value })
+            }
+          />
         </label>
         <br />
         <label>
-          Пароль <input type="password" />
+          Пароль{" "}
+          <input
+            type="password"
+            required
+            onChange={(event) =>
+              setPerconalData({ password: event.target.value })
+            }
+          />
         </label>
         <br />
         <label>
-          Е-майл <input type="emall" />
+          Е-майл{" "}
+          <input
+            type="email"
+            onChange={(event) => setPerconalData({ email: event.target.value })}
+          />
         </label>
         <br />
         <label>
-          Город <input type="text" />
+          Город{" "}
+          <input
+            type="text"
+            onChange={(event) => setPerconalData({ city: event.target.value })}
+          />
         </label>
         <br />
         <input type="submit" value="Зарегистрировать" />
       </form>
     </div>
   );
+  return { mainForm };
 }
 
 export default App;
